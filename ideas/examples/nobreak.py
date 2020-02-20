b3256a6ceb0534f8816436c7205b2e652f434307
André Roberge <andre.roberge@gmail.com>
2020-02-20 13:45:00 -0400

    adding nobreak

---
+++ ideas/examples/nobreak.py
@@ -0,0 +1,77 @@
+"""This module enables someone to use ``nobreak`` as a keyword
+   equivalent to ``else`` in ``for`` and ``while`` loops.
+
+"""
+from ideas import import_hook, token_utils
+
+
+def print_info(kind, source):
+    """Prints the source code.
+
+    ``kind`` is usually either ``"Original"`` or ``"Transformed"``
+    """
+    print(f"==========={kind}============")
+    print(f"|{source}|")
+    print("-----------------------------")
+
+
+def transform_source(source, callback_params=None, **kwargs):
+    """This function is called by the import hook loader with the named keyword
+       that we specified when we created the import hook.
+
+       It gives us the option to compare the original source and the transformed
+       one. This type of additional option can be useful when debugging
+       a source transformer. Furthermore, if we wish to define a source
+       transformation that combines the effect of multiple existing
+       transformations, we can combine the existing "inner" functions to
+       create our new transformation.
+    """
+    if callback_params is not None:
+        if callback_params["show_original"]:
+            print_info("Original", source)
+
+    source = nobreak_as_a_keyword(source)
+
+    if callback_params is not None:
+        if callback_params["show_transformed"]:
+            print_info("Transformed", source)
+    return source
+
+
+def nobreak_as_a_keyword(source):
+    """``nobreak`` is replaced by ``else`` only if it is the first
+    non-space token on a line and if its indentation matches
+    that of a ``for`` or ``while`` block.
+    """
+    indentations = {}
+    lines = token_utils.get_lines(source)
+    new_tokens = []
+    for line in lines:
+        first = token_utils.get_first(line)
+        if first is None:
+            new_tokens.extend(line)
+            continue
+        if first == "nobreak":
+            if first.start_col in indentations:
+                if indentations[first.start_col] in ["for", "while"]:
+                    first.string = "else"
+        else:
+            indentations[first.start_col] = first.string
+        new_tokens.extend(line)
+
+    return token_utils.untokenize(new_tokens)
+
+
+def add_hook(show_original=False, show_transformed=False, verbose_finder=False):
+    """Creates and automatically adds the import hook in sys.meta_path"""
+    callback_params = {
+        "show_original": show_original,
+        "show_transformed": show_transformed,
+    }
+    hook = import_hook.create_hook(
+        transform_source=transform_source,
+        callback_params=callback_params,
+        name=__name__,
+        verbose_finder=verbose_finder,
+    )
+    return hook
---
+++ tests/nobreak/test_nobreak.py
@@ -0,0 +1,38 @@
+from ideas.examples import nobreak
+# from ideas.import_hook import remove_hook
+
+
+callback_params = {"show_original": True, "show_transformed": True}
+
+
+def test_for():
+    source_for = """
+    for i in range(10):
+        pass
+    %s:
+        pass"""
+
+    source = source_for % "nobreak"
+    result = nobreak.transform_source(source)
+    expected = source_for % "else"
+
+    assert result == expected, "nobreak with for"
+
+
+def test_while():
+    source_while = """
+    while True:
+        pass
+    %s:
+        pass"""
+
+    source = source_while % "nobreak"
+    result = nobreak.transform_source(source)
+    expected = source_while % "else"
+
+    assert result == expected, "nobreak with while"
+
+
+if __name__ == '__main__':
+    test_for()
+    print("test_nobreak was run successfully.")
