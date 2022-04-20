cfbc3dc8aeebab3afaff20a8aadd2d24c660287a
André Roberge <andre.roberge@gmail.com>
2022-04-20 19:42:52 -0300

    Improved example

--- ideas/examples/unnormalized_unicode.py
+++ ideas/examples/unnormalized_unicode.py
@@ -10,6 +10,7 @@ See https://github.com/aroberge/ideas/issues/13 for a reference.
 """
 import io
 import tokenize
+import token_utils
 import unicodedata
 import uuid
 
@@ -22,8 +23,18 @@ def transform_source(source, **_kwargs):
     """Transform names that would normally be 'normalized' by
     Python into different and unique variable names.
     """
-    # Note: we cannot use token_utils as it does the NKFC normalization
-    result = []
+    # We want to be able to identify when the source has been modified
+    # in case session.config.show_changes has been set to True.
+    # However, Python's tokenize module cannot untokenize faithfully:
+    # some spaces might be messed up.
+    # On the other hand, token_utils can untokenize perfectly, recovering
+    # the original source. Unfortunately for this case, it works
+    # with (already normalized) strings.
+    # So, we combine the two here, just to ensure that we can
+    # correctly identify later when a source has been modified.
+
+    new_strings = []
+    done_normalization = False
     g = tokenize.tokenize(io.BytesIO(source.encode()).readline)
     for token_type, token_string, _, _, _ in g:
         if token_type == tokenize.NAME:
@@ -31,9 +42,20 @@ def transform_source(source, **_kwargs):
             if normalized_name != token_string:
                 if token_string not in _NAMES_MAP:
                     _NAMES_MAP[token_string] = f"{normalized_name}_{uuid.uuid4().hex!s}"
-                token_string = _NAMES_MAP[token_string]
-        result.append((token_type, token_string))
-    return tokenize.untokenize(result).decode()
+                new_strings.append(_NAMES_MAP[token_string])
+                done_normalization = True
+            else:
+                new_strings.append(token_string)
+    if not done_normalization:
+        return source
+
+    new_tokens = []
+    tokens = token_utils.tokenize(source)
+    for token in tokens:
+        if token.is_name():
+            token.string = new_strings.pop(0)
+        new_tokens.append(token)
+    return token_utils.untokenize(new_tokens)
 
 
 def new_dir(obj=None):
