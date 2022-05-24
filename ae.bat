dd258036d63fd98792c3009d9121e8ac42d6deb9
André Roberge <andre.roberge@gmail.com>
2022-05-24 18:08:48 -0300

    Using poetry and removing Python 3.6 - see #35

--- ae.bat
+++ ae.bat
@@ -1,7 +1,6 @@
 echo off
 REM Default is Python 3.9
 
-if "%1"=="3.6" goto py_36
 if "%1"=="3.7" goto py_37
 if "%1"=="3.8" goto py_38
 if "%1"=="3.10" goto py_310
@@ -10,11 +9,6 @@ if "%1"=="units" goto units
 if "%1"=="ipython" goto ipython
 
 goto py_39
-
-
-:py_36
-venv-ideas3.6\scripts\activate
-goto end
 
 :py_37
 venv-ideas3.7\scripts\activate
