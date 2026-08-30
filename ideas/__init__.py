36b901ba88599ae3e918c466aef7c2e171052abf
André Roberge <andre.roberge@gmail.com>
2026-08-30 07:57:27 -0300

    Simplified __init__

--- ideas/__init__.py
+++ ideas/__init__.py
@@ -1,28 +1,20 @@
 __version__ = "0.2.0"
 
 from .session import current_state
-from .import_hook import create_hook  # noqa
+from .import_hook import create_hook
 
 
-def remove_hook(hook):
-    current_state.remove_hook(hook)
+__all__ = [
+    "create_hook",
+    "current_state",
+    "disable_hook",
+    "enable_hook",
+    "list_hooks",
+    "remove_hook",
+]
 
 
-def disable_hook(hook):
-    """Disable a given import hook. Use name="*" as a shortcut for
-    disabling all hooks.
-
-    Since many of the import hooks are found in the ideas.examples directory
-    one can use "module_name" as an abbreviation of "ideas.examples.module_name".
-    """
-    current_state.disable_hook(hook)
-
-
-def enable_hook(hook):
-    """Enable a given import hook. Use name="*" as a shortcut for
-    Enabling all hooks.
-
-    Since many of the import hooks are found in the ideas.examples directory
-    one can use "module_name" as an abbreviation of "ideas.examples.module_name".
-    """
-    current_state.enable_hook(hook)
+remove_hook = current_state.remove_hook
+disable_hook = current_state.disable_hook
+enable_hook = current_state.enable_hook
+list_hooks = current_state.list_hooks
