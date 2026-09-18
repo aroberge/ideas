f06702414bedeea93be242d4f1e59dd610babec0
André Roberge <andre.roberge@gmail.com>
2026-09-18 16:42:00 -0300

    Fixing up a test file from the docs which had been mostly commented-out

--- docs_examples/export_hub/utils.py
+++ docs_examples/export_hub/utils.py
@@ -1,25 +1,25 @@
 # # flake8: noqa
 # export_hub/utils.py
-# import inspect
+import inspect
 
 
-# export def pdir(obj=None):  # currently meant for internal use by the Team.
-#     """Returns the contents of ``__all__`` if available,
-#     if not returns what ``dir`` would."""
+export def pdir(obj=None):  # currently meant for internal use by the Team.
+    """Returns the contents of ``__all__`` if available,
+    if not returns what ``dir`` would."""
 
 
-#     if obj is not None:
-#         if hasattr(obj, "__all__"):
-#             return obj.__all__
-#         return dir(obj)
+    if obj is not None:
+        if hasattr(obj, "__all__"):
+            return obj.__all__
+        return dir(obj)
 
-#     caller_frame = inspect.currentframe().f_back
-#     caller_locals = caller_frame.f_locals if caller_frame else {}
-#     if obj is None:
-#         if "__all__" in caller_locals:
-#             return caller_locals["__all__"]
-#         else:
-#             return list(caller_locals)  # only the keys
+    caller_frame = inspect.currentframe().f_back
+    caller_locals = caller_frame.f_locals if caller_frame else {}
+    if obj is None:
+        if "__all__" in caller_locals:
+            return caller_locals["__all__"]
+        else:
+            return list(caller_locals)  # only the keys
 
 export def useful(): print("I'm useful.")
 
