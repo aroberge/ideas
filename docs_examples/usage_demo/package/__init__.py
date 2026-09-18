e395f76b910b507a13a1fb38d6a3046ee7984673
André Roberge <andre.roberge@gmail.com>
2026-09-18 17:27:39 -0300

    Adding working example, and minor fixes.

---
+++ docs_examples/usage_demo/package/__init__.py
@@ -0,0 +1,4 @@
+print("__init__.py")
+double = function x : 2*x
+
+print(double(7))
---
+++ docs_examples/usage_demo/package/__main__.py
@@ -0,0 +1,7 @@
+from package import double
+
+print("__main__.py")
+sq = function x: x*x
+print(sq(2))
+
+print(double(9))
